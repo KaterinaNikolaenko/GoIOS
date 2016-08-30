4876c4a3c08bf385a6c3298504251425b7c33adf
Katerina <katya-nik@list.ru>
2016-08-30 13:59:48 +0300

    new

--- GoGallery/ViewController.m
+++ GoGallery/ViewController.m
@@ -92,74 +92,20 @@ forRowAtIndexPath:(NSIndexPath *)indexPath {
   
     [self tableView:tableView heightForRowAtIndexPath:indexPath];
     
-   // UIImage *img = [UIImage imageNamed:@"tfss-03113753-cc88-44aa-8c56-ec9ef81a68f7-_37_X_34_cm.jpg"];
-
+     //UIImage *image = [UIImage imageNamed:@"tfss-03113753-cc88-44aa-8c56-ec9ef81a68f7-_37_X_34_cm.jpg"];
     
-    
-    
-    
-    EventsModel *evObject =[[EventsModel alloc] init];
-    NSMutableArray *exArray = [NSMutableArray new];
-    NSMutableArray *masterPieceArray = [NSMutableArray new];
-    NSNull *myNull = [NSNull null];
-    for (Exhibition *dic in evObject.events){
-        if (dic.authorName == nil) {
-            [exArray addObject:myNull];
-        } else {
-            [exArray addObject:dic.authorName];
-        }
-        if (!(dic.masterPieces.firstObject == nil)){
-             MasterPiece *mas = dic.masterPieces[0];
-            if (mas.imgPict == nil) {
-                [masterPieceArray addObject:myNull];
-            } else {
-                [masterPieceArray addObject:mas.imgPict];
-            }
-        }
+    NSArray *exArray = [EventsModel sharedModel].events;
+    Exhibition * exObject = exArray[indexPath.row];
+    cell.nameGallery.text = exObject.venue.name;
+    cell.nameExhibition.text = exObject.name;
+    cell.nameAuthor.text = exObject.authorName;
+    if (!(exObject.masterPieces.firstObject == nil)){
+        MasterPiece *masterPiece = exObject.masterPieces[0];
+        NSURL *url = masterPiece.imgPict;
+        NSData *data = [NSData dataWithContentsOfURL:url];
+        UIImage *image = [UIImage imageWithData:data];
+        cell.imageCell.image = image;
     }
-    
-    NSInteger index = [indexPath row];
-    Event *ev = evObject.events[index];
-    
-    cell.nameGallery.text = ev.venue.name;
-    cell.nameExhibition.text = ev.name;
-    cell.nameAuthor.text = exArray[index];
-  ////cell.imageCell.image = img;
-    
-   // UIImageView *av = [[UIImageView alloc] initWithFrame:CGRectMake(20, 20, 277, 58)];
-   // av.backgroundColor = [UIColor clearColor];
-   // av.opaque = NO;
-    
-    NSURL *url = masterPieceArray[index];
-    NSData *data = [NSData dataWithContentsOfURL:url];
-    UIImage *image = [UIImage imageWithData:data];
-   // av.image = image;
-    
-    ////av.image = [UIImage imageNamed:@"tfss-0528dbc9-0efd-4aa2-9a42-325505f20c01-dition_of_6.jpg"];
-    //cell.backgroundView = av;
-    cell.imageCell.image = image;
-    
-    /////////////////////////////////////////////////////
-    //if (!cell) {
-    //    cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"Cell"];
-   // }
-   //// EventsModel *evObject =[[EventsModel alloc] init];
-   // NSMutableArray *exArray = [NSMutableArray new];
-   // NSNull *myNull = [NSNull null];
-   // for (Exhibition *dic in evObject.events){
-    //    if (dic.authorName == nil) {
-    //        [exArray addObject:myNull];
-    //    } else {
-    //        [exArray addObject:dic.authorName];
-    //    }
-   // }
-    
-   // NSInteger index = [indexPath row];
-   // Event *ev = evObject.events[index];
-    
-   // cell.textLabel.text = exArray[index];
-   // cell.detailTextLabel.text = ev.venue.name;
-    
     
     return cell;
 }
